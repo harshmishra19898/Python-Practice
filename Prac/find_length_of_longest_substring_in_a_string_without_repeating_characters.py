0f1c6419ece3a77df835067d93250ceae72799d7
harshmishra19898 <harshmishra19898@gmail.com>
2024-12-08 15:36:22 +0530

    find length of  largest substring in string  without repeating characters  Question and solution loaded

---
+++ Prac/find_length_of_longest_substring_in_a_string_without_repeating_characters.py
@@ -0,0 +1,72 @@
+'''
+Q2)find length of  largest substring in string  without repeating characters  ex [aabzbcca]
+Substring - a contigious part of a string
+'''
+
+'''
+a - a
+a - a, ab, abz
+b-  b, bz 
+z - z, zb, zbc
+b - b, bc 
+c-  c
+c - c, ca
+a-  a
+'''
+
+input = "aabbcca"
+
+# find all non repeating sub strings for all indexes 
+# how to check if given substring contains all unqiue alphabets?-> cont occurences of each element
+subtr_list=[]
+n=len(input)
+for i in range(0,n):
+    substr=""
+    for j in range(i,n):
+        substr=input[i:j]
+        #print(substr,j,"j")
+        p=""
+        count_char_dic={}
+        for k in range(0,len(substr)):
+           
+            if substr[k] in p:
+                
+                count_char_dic[substr[k]]+=1
+            else:
+                p+=substr[k]
+                count_char_dic[substr[k]]=1
+
+        #print(count_char_dic)
+        value_list=[]
+        for key, value  in count_char_dic.items():
+            value_list.append(value)
+        greatest_val = 0
+        for val in value_list:
+            if val > greatest_val:
+                greatest_val = val 
+        if greatest_val != 1:
+            pass
+        else:
+            subtr_list.append(substr)
+
+            
+print(subtr_list)
+subs_len_list=[]
+for subs in subtr_list:
+    subs_len_list.append(len(subs))
+
+greatest_no=0
+for no in subs_len_list:
+    if no > greatest_no:
+        greatest_no = no
+print(greatest_no)
+print(set(subtr_list))
+
+          
+
+
+
+
+
+
+
